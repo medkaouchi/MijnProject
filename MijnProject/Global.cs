947de3d693ef89cd422ba341809b511064b626a1
kaouchi Mohamed <med_kaouchi@hotmail.com>
2021-05-31 16:01:12 +0200

    Mooi maken

---
+++ MijnProject/Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MijnProject
+{
+   public static class Global
+    {
+        public static void ModifyForm(Form form)
+        {
+
+        }
+    }
+}
